3be0f13dd51e3c46a7c9eb9c82b294cf53832ce5
wushuaibuaa <ws.horton@gmail.com>
2018-03-23 18:14:46 +0800

    update files

--- 1-Basic Equation/Models/NewtonCooling/momodel_types.h
+++
@@ -1,5 +0,0 @@
-#ifndef MWSOLVER_TYPES_DEFINE_H
-#define MWSOLVER_TYPES_DEFINE_H
-
-#define model_guid "{c038af25-bb54-4ca1-b453-e9187821b618}"
-#endif /*MWSOLVER_TYPES_DEFINE_H*/
--- 1-Basic Equation/Models/SecondOrderSystem/momodel_types.h
+++
@@ -1,5 +0,0 @@
-#ifndef MWSOLVER_TYPES_DEFINE_H
-#define MWSOLVER_TYPES_DEFINE_H
-
-#define model_guid "{16663e03-502a-48ad-a71f-f258ecd6f95a}"
-#endif /*MWSOLVER_TYPES_DEFINE_H*/
